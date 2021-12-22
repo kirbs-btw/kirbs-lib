9c2157ab34c3df854815c7336074d8ede951841d
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2021-12-22 14:14:23 +0100

    Add files via upload

--- lib/tkextended.py
+++ lib/tkextended.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
+
 def destroyInnerWidgets(frame):
     """ destroys every widget inside the frame, argument needs to be a tk.Canvas()"""
     widgets = frame.winfo_children()
     for widget in widgets:
         widget.destroy()
+
 
 def scrollBar(table):
     """scrollBar need a frame as argument, adjust frame for scrollbar size"""
@@ -25,4 +27,50 @@ def scrollBar(table):
     # add frame to window in canvas
     canvasMainFrame.create_window((0, 0), window=frameInCanvas, anchor="nw")
 
-#Bastian Lipka
+
+def refreshWindow(root, window):
+    """
+    Switches Window previous root gets destroyed new window gets started
+
+    :param root: old root that will be destroyed
+    :param window: name of the new window to be started expand the if
+    :return: none - starts new window
+    """
+
+    root.destroy()
+    if window == "main":
+        # some function to start the new window
+        # expl. tableWindow()
+        pass
+
+
+def separateData(paths):
+    """
+    Separates Data of a tkdnd menu content is saved as {some path...}
+
+    :param paths: paths = tk.dnd event input is a big string divided through "{" and "}"
+    :return: returns a clear list of paths
+    """
+
+    if "{" in paths:
+        pathList = []
+        keepWord = ""
+        wordStart = False
+
+        for letter in paths:
+            if letter == "{":
+                wordStart = True
+            elif letter == "}":
+                pathList.append(keepWord)
+                keepWord = ""
+                wordStart = False
+            elif wordStart:
+                keepWord = keepWord + letter
+    else:
+        pathList = [paths]
+
+    print("\033[32m" "separateData:    DONE" "\033[0m")
+    return pathList
+
+# Bastian Lipka
+# https://github.com/kirbs-btw
